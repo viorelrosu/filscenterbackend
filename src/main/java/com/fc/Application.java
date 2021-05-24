ba6cf77e5bd686c433415b5023eb85548c494c4c
jagonmes <65248503+jagonmes@users.noreply.github.com>
2021-05-24 19:26:41 +0200

    WAR

--- src/main/java/com/fc/Application.java
+++ src/main/java/com/fc/Application.java
@@ -3,11 +3,18 @@ package com.fc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class } )
-public class Application {
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class Application extends SpringBootServletInitializer {
 
-  public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
